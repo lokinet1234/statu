bf26ee62c825c3cb05f39f46771fc90e539b3a62
Mast <regiments@icloud.com>
2025-07-06 16:27:20 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// This script.js file is currently empty because the horizontal scrolling
+// effect is achieved purely using CSS animations for better performance.
+// You can add any interactive JavaScript functionality here if needed in the future.
+
+console.log("Website loaded. Scrolling text is handled by CSS.");
